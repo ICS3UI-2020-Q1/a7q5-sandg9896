2cfdad139fdc43f00781521698620e2ba3626503
sandg9896 <sandg9896@wrdsb.ca>
2020-10-14 12:55:40 +0000

    FirstNumber

--- Main.java
+++ Main.java
@@ -1,15 +1,31 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @ Gavin Sandhar 
  */
 public class Main {
-
+  public static double firstDigit(int number){
+    int division = number / 10;
+    while(number > 9){
+     number = number / 10;
+    } System.out.println(number);
+    return number;
+   } 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Create scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask user to enter a number
+    System.out.println("Please enter a number");
+    int number = input.nextInt();
+
+    firstDigit(number);
+
+  
     
   }
 }
